c914386956cfd1d6d01244644bda2d0472a3fcd1
Thomas Eriksson <thomas.eriksson@gmail.com>
2012-12-13 07:51:08 +0100

    update build script

--- makefont.sh
+++ makefont.sh
@@ -3,7 +3,6 @@ SERIF_BASEFONT="$PWD/orig/cmunrm.sfd"
 SANS_BASEFONT="$PWD/orig/cmunss.sfd"
 MING_CJKCOMPLEMENTFONT="$PWD/orig/cwming.ttf"
 YEN_CJKCOMPLEMENTFONT="$PWD/orig/cwyen.ttf"
-GLIFDIR=glifs
 COPYRIGHT="
 Computer Modern Mono is a monospace composite conversion of cwTeX and CMU. (c) Copyright Thomas Eriksson 2012
 
@@ -11,15 +10,9 @@ Computer Modern Unicode: Copyright: Converted by Andrey V. Panov from TeX fonts.
 
 cwTeX TTF: (c) Copyright 2005, Tsong-Min Wu, Tsong-Huey Wu and Edward G.J. Lee." 
  
-python monospacipy/monospaci.py -psname "CMMonoRomanMing" -fullname "Computer Modern Mono Roman Ming" -copyright "$COPYRIGHT" -nameslist monospacipy/NamesList.txt "$SERIF_BASEFONT"  $GLIFDIR/*_serif.glif "$MING_CJKCOMPLEMENTFONT" 
+python monospaci.py/monospaci.py -psname "CMMonoRomanMing" -fullname "Computer Modern Mono Roman Ming" -copyright "$COPYRIGHT" -nameslist monospaci.py/NamesList.txt "$SERIF_BASEFONT"  $GLIFDIR/*_serif.glif "$MING_CJKCOMPLEMENTFONT" 
 
-#python monospacipy/monospaci.py -psname "CMMonoRoman" -fullname "Computer Modern Mono Roman" -nameslist monospacipy/NamesList.txt "$SERIF_BASEFONT" 
-## $GLIFDIR/*_serif.glif
-
-#python monospacipy/monospaci.py -psname "CMMonoSansSerif" -fullname "Computer Modern Mono Sans Serif" -nameslist monospacipy/NamesList.txt "$SANS_BASEFONT" $GLIFDIR/*_sans.glif
-
-python monospacipy/monospaci.py -copyright "$COPYRIGHT" -psname "CMMonoSansSerifYen" -fullname "Computer Modern Mono Sans Serif Yen " -nameslist monospacipy/NamesList.txt "$SANS_BASEFONT"  "$YEN_CJKCOMPLEMENTFONT" 
-# $GLIFDIR/*_sans.glif
+python monospaci.py/monospaci.py -copyright "$COPYRIGHT" -psname "CMMonoSansSerifYen" -fullname "Computer Modern Mono Sans Serif Yen " -nameslist monospaci.py/NamesList.txt "$SANS_BASEFONT"  "$YEN_CJKCOMPLEMENTFONT" 
 
 rm *Output.ttf
 fontlint *.ttf
